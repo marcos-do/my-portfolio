91a82019d53a2175b244309b318403287c54b872
Marcos <marcos.de.oliveira.madeira@gmail.com>
2024-07-18 17:12:03 -0300

    Agrega .env

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -1,18 +1,12 @@
 import { getDescription } from "@/app/services/descriptionService";
 
-async function getData(): Promise<any> {
-  const response = await getDescription();
-  return response.message
-}
-
 export default async function Home() {
-  const description = await getData()
 
   return (
     <main className="flex min-h-screen flex-col items-left p-24">
-      <h1>Marcos de Oliveira</h1>
+      <h1>Project A</h1>
       <p className="p-12">
-        {description}
+        This is project A
       </p>
     </main>
   );
--- frontend/src/app/services/descriptionService.ts
+++ frontend/src/app/services/descriptionService.ts
@@ -1,4 +1,4 @@
 export const getDescription = async () => {
-    const response = await fetch("http://127.0.0.1:8000/description");
+    const response = await fetch(process.env.BACKEND_HOST + "/description");
     return response.json();
 }
