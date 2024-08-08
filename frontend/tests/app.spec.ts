e080ccf8c2f922cc28309fe186c5308957a3eea3
Marcos <marcos.de.oliveira.madeira@gmail.com>
2024-08-08 20:38:59 -0300

    Adds dotenv to Playwright

--- frontend/tests/app.spec.ts
+++ frontend/tests/app.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect } from '@playwright/test';
-
-const SITE = "http://localhost:3000"
+import { fail } from 'assert';
 
 test('has title', async ({ page }) => {
-  await page.goto(SITE);
+  if (!process.env.FRONTEND_HOST) {
+    return fail("No host");
+  }
+  await page.goto(process.env.FRONTEND_HOST);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Marcos de Oliveira/);
